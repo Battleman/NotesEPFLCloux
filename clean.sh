8406591861766f6a81a80f8fc863a43a8a66f165
Olivier Cloux <olivier.cloux@epfl.ch>
2020-07-21 09:42:20 +0200

    clean also removed bbl and blg

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f \( -name "*.aux" -o -name "*.synctex.gz" -o -name "*.log" -o -name "*.out" -o -name "*.toc" -o -name ".hidden" -o -name "*.idx" \) -exec rm {} \;
+find . -type f \( -name "*.aux" -o -name "*.synctex.gz" -o -name "*.log" -o -name "*.out" -o -name "*.toc" -o -name ".hidden" -o -name "*.idx" -o -name "*.bbl" -o -name "*.blg" \) -exec rm {} \;
